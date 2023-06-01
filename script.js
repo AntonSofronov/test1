3d9fe29cf54912e21656a14215035f2b2b71f0ea
Anton Sofronov <antonsofronov.dev@gmail.com>
2023-06-01 08:43:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// const number = 5;
+// console.log(number);
+
+// console.log(whatsup);
+
+// Объекты..................................................
+
+// const obj = {
+// 	name: 'Anton',
+// 	age: 37,
+// 	isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj.isMarried);
+
+// Массив - частный случай объекта !!!
+// Массив - печень информации по порядку.
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[5]);
+
+// const obj = {a:1, b:2, c:3} 
+//объект, где а - ключ, 1 - значение.
+
+/*let storeName = 'Kudzi';
+const storeDescription = {
+	budget: 10000,
+	employees: ['Ann', 'Mike', 'Suzan'],
+	ptoducts: {
+		'milk': 50,
+		'bread': 25
+	},
+	open: true
+};*/
+
+
+// Общение с пользователем..................................................
+
+// alert('Hello!');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '');
+// console.log(answer);
+
+// !!!! Вся информация от пользователя приходит в виде строк !!!!
+
+// Интерполяция.............................................................
+
+// const user = 'Иван';
+// console.log(`Привет, ${user}`);
+
+//  `` - бэктики
+
+// Операторы................................................................
+
+//  +4 - унарный плюс
+
+// console.log(4 + +'4');
+
+// let incr = 10,
+// 	decr = 10;
+
+// incr++; // оператор инкремента, увелечение на 1
+// decr--; // оператор декремента, уменьшение на 1
+
+// const isCheked = true;
+// 	  isClose = true;  
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
